b674a998a69a4255176c2c9f2652739c3ce5f19b
Võ Anh Kiệt <anhkiet1227@gmail.com>
2023-09-27 21:52:16 +0700

    backup

---
+++ genDataset/svm.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+# Load the dataset
+dataset_file = 'synthetic_smart_contract_dataset.csv'
+data = pd.read_csv(dataset_file)
+
+# Features (X) and labels (y)
+X = data.drop(['Contract Name', 'Smart Contract Code', 'Reentrancy Vulnerability', 'Overflow/Underflow Vulnerability', 'Unprotected Ether Withdrawal'], axis=1)
+
+# Binarize the labels
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(data['Reentrancy Vulnerability'])
+
+# Standardize the features
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Build the SVM model
+model = SVC(random_state=42)
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Predict on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+print('Accuracy:', accuracy_score(y_test, y_pred))
+print('Classification Report:\n', classification_report(y_test, y_pred))
+print('Confusion Matrix:\n', confusion_matrix(y_test, y_pred))
