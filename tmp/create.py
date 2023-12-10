ea1bdd78c0724143a26bdfb362703b3f81ed86e7
Võ Anh Kiệt <anhkiet1227@gmail.com>
2023-12-10 21:32:31 +0700

    backup

--- tmp/create.py
+++
@@ -1,6 +0,0 @@
-for n in range(249, 300):
-    filename = f"Contract{n}.sol"
-    with open(filename, 'w') as file:
-        # Write content to the file if needed
-        file.write("")
-    print(f"File '{filename}' created successfully.")
